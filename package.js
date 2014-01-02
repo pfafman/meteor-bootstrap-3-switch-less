d1a5a0ddfbef013ab067b14d0b07100516eead7d
Timothy Pfafman <tim@pfafman.com>
2014-01-01 19:05:31 -0800

    found combination that works

--- package.js
+++ package.js
@@ -1,9 +1,32 @@
+
 Package.describe({
     summary: "Bootstrap Switch, packaged for Meteor using less."
 });
 
 Package.on_use(function (api, where) {
-    api.use('jquery', 'client');
-    api.use('less','client');
-    api.add_files('lib/js/bootstrap-switch.js', 'client');
+
+    
+    api.use([
+        'jquery', 
+        'less', 
+        'coffeescript'
+    ], 'client');
+
+    api.add_files([
+        //'lib/less/bootstrap3/variables.less',
+        //'lib/less/bootstrap3/mixins.less',
+        //'lib/less/bootstrap3/bootstrap-switch.less',
+        'lib/coffee/bootstrap-switch.coffee'
+    ], 'client');
+    
+    /*
+    api.use([
+        'jquery'
+    ], 'client');
+
+    api.add_files([
+        'lib/css/bootstrap3/bootstrap-switch.css',
+        'lib/js/bootstrap-switch.js'
+    ], 'client');
+    */
 });
